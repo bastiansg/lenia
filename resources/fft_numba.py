435e3c1e4cdf4e60266101b4270088ebe44a2e99
Damian <damixd02@gmail.com>
2024-12-08 20:29:16 +0100

    fft python done

--- resources/fft_numba.py
+++ resources/fft_numba.py
@@ -1,43 +1,52 @@
+import os
+import shutil
+from concurrent.futures import ProcessPoolExecutor
+from typing import Any, Callable, Sequence, Union, cast
+
 import matplotlib.pyplot as plt
 import numpy as np
-from Utils import ORBIUM, DisplayCells, rle2arr, upscale_array_manually  # type: ignore
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from numpy._typing import NDArray
+from Utils import ORBIUM, rle2arr, upscale_array_manually  # type: ignore
 
-SCALE = 3
-R = 14
-B = np.array([1.0], dtype=np.float32)
-MU = 0.15
-SIGMA = 0.015
+SCALE = 10
+R = 13
+B = np.array([1.0], dtype=np.float64)
+MU = np.float64(0.15)
+SIGMA = np.float64(0.015)
 SIZE = R * SCALE
 
 
-def calc_kernel_at(r: np.float32) -> np.float32:
+def calc_kernel_at(r: np.float64) -> np.float64:
     Br = len(B) * (r / SIZE)
     modded = min(np.fmod(Br, 1), 1)
-    Kc = (modded > 0) * (modded < 1) * np.pow(4.0 * modded * (1.0 - modded), 4.0)
-    return (r < SIZE) * Kc
+    Kc: np.float64 = (modded > 0) * (modded < 1) * np.pow(4.0 * modded * (1.0 - modded), 4.0)
+    return np.float64(r < SIZE) * Kc
 
 
-def kernel_shell() -> np.ndarray:
-    res = np.zeros((SIZE * SIZE * 4), dtype=np.float32)
+def kernel_shell() -> NDArray[np.float64]:
+    res = np.zeros((SIZE * SIZE * 4), dtype=np.float64)
     for i in range(0, SIZE):
         for j in range(0, SIZE):
             val = calc_kernel_at(np.sqrt(i * i + j * j))
-            res[(SIZE - 0 + i) * SIZE * 2 + (SIZE - 0 + j)] = val
-            res[(SIZE - 0 + i) * SIZE * 2 + (SIZE - 1 - j)] = val
-            res[(SIZE - 1 - i) * SIZE * 2 + (SIZE - 0 + j)] = val
-            res[(SIZE - 1 - i) * SIZE * 2 + (SIZE - 1 - j)] = val
-    res /= res.size * 0.005917159763313609
+            res[(SIZE + i) * SIZE * 2 + (SIZE + j)] = val
+            res[(SIZE + i) * SIZE * 2 + (SIZE - j)] = val
+            res[(SIZE - i) * SIZE * 2 + (SIZE + j)] = val
+            res[(SIZE - i) * SIZE * 2 + (SIZE - j)] = val
+    res = res / np.float64(res.sum())
     return res.reshape((SIZE * 2, SIZE * 2))
 
 
-def display_multiple(*fields) -> None:
+def display_multiple[T](*fields: T) -> Figure:
     n_fields = len(fields)
     cols = 2
     rows = (n_fields + cols - 1) // cols
 
-    fig, axes = plt.subplots(rows, cols, figsize=(10, 8))
-
+    axes: np.ndarray[Axes, Any]
+    fig, axes = plt.subplots(rows, cols, figsize=(12, 8))
     for i, ax in enumerate(axes.flat):
+        ax = cast(Axes, ax)
         if i < n_fields:
             im = ax.imshow(fields[i], cmap="magma")
             fig.colorbar(im)
@@ -45,37 +54,49 @@ def display_multiple(*fields) -> None:
             ax.axis("off")
 
     plt.tight_layout()
-    plt.show()
+    return fig
 
 
-def plot():
-
-    xpoints = np.arange(0, 1, 0.01)
-    ypoints = growth_func(xpoints)
-
-    plt.plot(xpoints, ypoints)
-    plt.show()
+def save_plot[*T](variable_plot: Callable[[*T], Figure], arg_tuple: tuple[*T], output_dir: str, i: int) -> None:
+    fig = variable_plot(*arg_tuple)
+    fig.savefig(fname=output_dir + f"{str(i).zfill(6)}.jpg", dpi=200)
+    plt.close(fig)
 
 
-def growth_func(field: np.ndarray) -> np.ndarray:
-    return np.maximum(0, 1 - ((field - MU) ** 2 / (9 * SIGMA**2))) ** 4 * 2 - 1
+def animate_fig[*T](variable_plot: Callable[[*T], Figure], arg_tuples: Sequence[tuple[*T]], out_dir: str, out_name: str) -> None:
+    """Creates a gif from a plot function that takes one or more variables."""
+    shutil.rmtree(out_dir, ignore_errors=True)
+    os.mkdir(out_dir)
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        futures = [executor.submit(save_plot, variable_plot, arg_tuple, out_dir, i) for i, arg_tuple in enumerate(arg_tuples)]
+        for future in futures:
+            future.result()
+    os.system(f"ffmpeg -hwaccel cuda -framerate 24 -i {out_dir[:-1]}/%06d.jpg -c:v hevc_nvenc -r 24 -pix_fmt yuv420p {out_name} -y")
+    os.system(".\\" + out_name)
+
+
+def growth_func(field: NDArray[np.float64]) -> NDArray[np.float64]:
+    return np.maximum(0, 1.0 - ((field - MU) ** 2.0 / (9.0 * SIGMA**2.0))) ** 4.0 * 2.0 - 1.0
 
 
 def main() -> None:
     kernel = kernel_shell()
-    field = np.zeros((100, 100))
-    cells = rle2arr(ORBIUM)
-    field[20:40, 20:40] = cells
+    field = np.zeros((50 * SCALE, 50 * SCALE))
+    cells = upscale_array_manually(rle2arr(ORBIUM), SCALE)
+    start = (0, 0)
+    field[start[0] : start[0] + cells.shape[0], start[1] : start[1] + cells.shape[0]] = cells
     padded = np.pad(kernel, pad_width=(field.shape[0] - kernel.shape[0]) // 2, mode="constant", constant_values=0)
     fft_kernel = np.fft.fft2(padded)
+    outputs = []
+    for _ in range(500):
+        inv = np.fft.ifft2(np.fft.fft2(field) * fft_kernel)
+        shifted = np.fft.fftshift(np.real(inv))
+        growth = 0.1 * growth_func(shifted)
+        field = np.clip(field + growth, 0, 1)
+        outputs.append((field, np.imag(inv), shifted, growth))
 
-    for i in range(5):
-        fft_field = np.fft.fft2(field)
-        fft_field *= fft_kernel
-        fft_field = np.fft.fftshift(np.real(np.fft.ifft2(fft_field)))
-        field = np.clip(field + 0.1 * growth_func(fft_field), 0, 1)
-
-    display_multiple(kernel, padded, field, np.real(fft_field))
+    print("start render")
+    animate_fig(display_multiple, outputs, out_dir="resources/figs/", out_name="resources\\fft.mp4")
 
 
 if __name__ == "__main__":
